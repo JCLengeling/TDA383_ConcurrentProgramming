5dcb10a2301b2dd708d0bd2330512ccb710e465e
Joel Cedric Lengeling <joelce@ed3358-05.ed3358.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2015-09-08 15:34:10 +0200

    Modified map, added station behaviour

--- Lab1/Solution/Lab1.java
+++ Lab1/Solution/Lab1.java
@@ -30,6 +30,7 @@ public class Lab1 {
 class Train implements Runnable {
 	private TSimInterface tsi;
 	private int id, speed;
+	private boolean inStation = true;
 	public Train(TSimInterface tsi,int id, int speed)throws CommandException {
 		this.tsi = tsi;		
 		this.speed = speed;
@@ -45,7 +46,42 @@ class Train implements Runnable {
 	public void run() {
 		while(true) {
 		 try {
-			 System.err.println(tsi.getSensor(this.id).toString()); 
+			 SensorEvent event = tsi.getSensor(this.id);
+			 if(this.id == 1) System.err.println(event.toString()); 
+			 
+			 
+			 
+			 
+			 int tmpY = 0, tmpX= 0, status = -1 ;
+			 tmpY = event.getYpos();
+			 tmpX = event.getXpos();
+			 status = event.getStatus();
+			 if(tmpX == 16 ){
+				 if (tmpY == 3 || tmpY ==  5 || tmpY == 11 || tmpY==13 ){
+					 if (status == 2)
+						 inStation = false;
+					 else
+						 inStation = true;
+				 }
+				 
+				 if (tmpY == 7) {
+					 if(status == 2) {Thread.sleep(1000);this.tsi.setSwitch(17,7,1);}
+					 else {
+						 
+						 this.tsi.setSwitch(17,7,2);
+						 }
+				 }
+			 }
+			 
+			 
+			 
+			 if (inStation ){
+				 this.tsi.setSpeed(this.id, 0);
+				 Thread.sleep(2*20*Math.abs(this.speed) +1500);
+				 this.speed = this.speed *-1;
+				 this.tsi.setSpeed(this.id,this.speed);
+			 }
+			 
 			 }
 		 catch (Exception e) {
 			 	System.err.println(e);
