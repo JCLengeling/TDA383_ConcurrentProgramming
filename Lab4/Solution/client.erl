3f9e7db1dae265d202aa1c86d3d7e4523889d6f5
Joel Cedric Lengeling <joelce@ed5355-06.ed5355.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2015-10-08 12:18:02 +0200

    Temporary for syntax error search

--- Lab4/Solution/client.erl
+++ Lab4/Solution/client.erl
@@ -1,27 +1,27 @@
 -module(client).
--export([loop/2, create_state/4, initial_state/2, list_contain/2, list_add/2,  list_isEmpty/1]).
+-export([loop/2, create_state/5, initial_state/2, list_contain/2, list_add/2,  list_isEmpty/1]).
 -include_lib("./defs.hrl").
 
 %% Produce initial state
 initial_state(Nick, GUIName) ->
-  #client_st{gui = GUIName, nickname = Nick, serverName = "", list_chatRoom = []}.
+  #client_st{gui = GUIName, nickname = Nick, serverName = "", machineName = "", list_chatRoom = []}.
 
-create_state(Nick, GUIName, ServerName, List_chatRoom) ->
-  #client_st{gui = GUIName, nickname = Nick, serverName = ServerName, list_chatRoom = List_chatRoom}.
+create_state(Nick, GUIName, ServerName, MachineName, List_chatRoom) ->
+  #client_st{gui = GUIName, nickname = Nick, serverName = ServerName, machineName = MachineName, list_chatRoom = List_chatRoom}.
 
 %% ---------------------------------------------------------------------------
-
+%%/connect {shire, server@127.0.0.1}
 %% loop handles each kind of request from GUI+
 
-%% Connect to server
-loop(St, {connect, Server}) ->
-  case St#client_st.serverName == "" of
+%% Connect to Remote server
+loop(St, {connect, {Server,Machine}}) ->
+  case [St#client_st.serverName] == "" of
     false ->
       {{error, user_already_connected, "You are already connected to a server."}, St};
     true ->
-      case catch(  genserver:request(list_to_atom(Server), #data_trsmn{type = 0, data = [St#client_st.nickname]})) of
+      case catch(  genserver:request({list_to_atom(Server),list_to_atom(Machine)}, #data_trsmn{type = 0, data = [St#client_st.nickname]})) of
         ok ->
-          Stnew = create_state(St#client_st.nickname, St#client_st.gui, Server, St#client_st.list_chatRoom),
+          Stnew = create_state(St#client_st.nickname, St#client_st.gui, Server, Machine, St#client_st.list_chatRoom),
           {ok, Stnew};
         username_already_connected ->
           {{error, user_already_connected, "Username is already taken on server."}, St};
@@ -30,15 +30,17 @@ loop(St, {connect, Server}) ->
       end
   end;
 
-%% Connect to Remote server
-loop(St, {connect, {Server,Machine}}) ->
+
+
+%% Connect to server
+loop(St, {connect, Server}) -> 
   case St#client_st.serverName == "" of
     false ->
       {{error, user_already_connected, "You are already connected to a server."}, St};
     true ->
-      case catch(  genserver:request(list_to_atom({Server,Machine}), #data_trsmn{type = 0, data = [St#client_st.nickname]})) of
+      case catch(  genserver:request(list_to_atom(Server), #data_trsmn{type = 0, data = [St#client_st.nickname]})) of
         ok ->
-          Stnew = create_state(St#client_st.nickname, St#client_st.gui, Server, St#client_st.list_chatRoom),
+          Stnew = create_state(St#client_st.nickname, St#client_st.gui, Server, "", St#client_st.list_chatRoom),
           {ok, Stnew};
         username_already_connected ->
           {{error, user_already_connected, "Username is already taken on server."}, St};
@@ -59,16 +61,31 @@ loop(St, disconnect) ->
         false ->
           {{error, leave_channels_first, "You cannot leave a server if you are still in a channel"}, St};
         true ->
-          Result = genserver:request(list_to_atom(St#client_st.serverName), #data_trsmn{type = 2, data = [St#client_st.nickname]}),
-          case Result == ok of
-            true ->
-              Stnew = create_state(St#client_st.nickname, St#client_st.gui, "", St#client_st.list_chatRoom),
-              {ok, Stnew};
-            false ->
-              {{error, server_not_reached, "Cannot reach the server"}, St}
-          end
+		case St#client_st.machineName == "" of
+			true ->
+				Result = genserver:request(list_to_atom(St#client_st.serverName), #data_trsmn{type = 2, data = [St#client_st.nickname]}),
+          			case Result == ok of
+            				true ->
+              					Stnew = create_state(St#client_st.nickname, St#client_st.gui, "", "", St#client_st.list_chatRoom),
+              					{ok, Stnew};
+            				false ->
+              					{{error, server_not_reached, "Cannot reach the server"}, St}
+          			end;
+			false ->
+				Result = genserver:request({list_to_atom(St#client_st.serverName), list_to_atom(St#client_st.machineName)  }, #data_trsmn{type = 2, data = [St#client_st.nickname]}),
+          			case Result == ok of
+            				true ->
+              					Stnew = create_state(St#client_st.nickname, St#client_st.gui, "", "", St#client_st.list_chatRoom),
+              					{ok, Stnew};
+            				false ->
+              				{{error, server_not_reached, "Cannot reach the server"}, St}
+          			end
+				
+		end
       end
   end;
+
+
 % Join channel
 loop(St, {join, Channel}) ->
   case list_to_atom(St#client_st.serverName) == list_to_atom("") of
@@ -79,17 +96,32 @@ loop(St, {join, Channel}) ->
         true ->
           {{error, user_already_joined, "You are already on this channel."}, St};
         false ->
-          Result = genserver:request(list_to_atom(St#client_st.serverName), #data_trsmn{type = 1, data = [Channel]}),
-          case Result of
-            ok ->
-              Result2 = genserver:request(list_to_atom(Channel), #data_trsmn{type = 0, data = [St#client_st.nickname, self()]}),
-              case Result2 of
-                ok ->
-                  NewList = list_add(St#client_st.list_chatRoom, Channel),
-                  Stnew = create_state(St#client_st.nickname, St#client_st.gui, St#client_st.serverName, NewList),
-                  {ok, Stnew}
-              end
-          end
+	  case St#client_st.machineName == "" of
+		true ->
+          		Result = genserver:request(list_to_atom(St#client_st.serverName), #data_trsmn{type = 1, data = [Channel]}),
+          		case Result of
+            			ok ->
+           	   			Result2 = genserver:request(list_to_atom(Channel), #data_trsmn{type = 0, data = [St#client_st.nickname, self()]}),
+              				case Result2 of
+                				ok ->
+                  					NewList = list_add(St#client_st.list_chatRoom, Channel),
+                  					Stnew = create_state(St#client_st.nickname, St#client_st.gui, St#client_st.serverName, St#client_st.machineName,  NewList),
+                  					{ok, Stnew}
+					end
+          		end;
+		false ->
+			Result = genserver:request({list_to_atom(St#client_st.serverName), list_to_atom(St#client_st.machineName) }, #data_trsmn{type = 1, data = [Channel]}),
+          		case Result of
+            			ok ->
+           	   			Result2 = genserver:request({list_to_atom(Channel),  list_to_atom(St#client_st.machineName) }, #data_trsmn{type = 0, data = [St#client_st.nickname, self()]}),
+              				case Result2 of
+                				ok ->
+                  					NewList = list_add(St#client_st.list_chatRoom, Channel),
+                  					Stnew = create_state(St#client_st.nickname, St#client_st.gui, St#client_st.serverName, St#client_st.machineName,  NewList),
+                  					{ok, Stnew}
+					end
+          		end
+           	end	
       end
   end;
 
@@ -100,13 +132,24 @@ loop(St, {leave, Channel}) ->
     false ->
       {{error, user_not_joined, "You cannot leave a channel you have not joined"}, St};
     true ->
-      Result = genserver:request(list_to_atom(Channel), #data_trsmn{type = 2, data = [St#client_st.nickname, self()]}),
-      case Result of
-        ok ->
-          NewList = lists:delete(Channel, St#client_st.list_chatRoom),
-          Stnew = create_state(St#client_st.nickname, St#client_st.gui, St#client_st.serverName, NewList),
-          {ok, Stnew}
-      end
+	case  St#client_st.machineName == "" of
+		true ->
+			Result = genserver:request(list_to_atom(Channel), #data_trsmn{type = 2, data = [St#client_st.nickname, self()]}),
+      			case Result of
+        			ok ->
+          				NewList = lists:delete(Channel, St#client_st.list_chatRoom),
+         				Stnew = create_state(St#client_st.nickname, St#client_st.gui, St#client_st.serverName, St#client_st.machineName,  NewList),
+          				{ok, Stnew}
+      			end;
+		false -> 
+			Result = genserver:request({list_to_atom(Channel),  list_to_atom(St#client_st.machineName) }, #data_trsmn{type = 2, data = [St#client_st.nickname, self()]}),
+      			case Result of
+        			ok ->
+          				NewList = lists:delete(Channel, St#client_st.list_chatRoom),
+         				Stnew = create_state(St#client_st.nickname, St#client_st.gui, St#client_st.serverName, St#client_st.machineName,  NewList),
+          				{ok, Stnew}
+      			end
+			
   end;
 
 % Sending messages
@@ -115,8 +158,14 @@ loop(St, {msg_from_GUI, Channel, Msg}) ->
     false ->
       {{error, user_not_joined, "You are not member of this channel"}, St};
     true ->
-      Result = genserver:request(list_to_atom(Channel), #data_trsmn{type = 3, data = [Msg, Channel, St#client_st.nickname]}),
-      {ok, St}
+	case  St#client_st.machineName == "" of
+		true ->
+			Result = genserver:request(list_to_atom(Channel), #data_trsmn{type = 3, data = [Msg, Channel, St#client_st.nickname]}),
+		 	{ok, St};
+		false ->
+			Result = genserver:request({list_to_atom(Channel),  list_to_atom(St#client_st.machineName)}, #data_trsmn{type = 3, data = [Msg, Channel, St#client_st.nickname]}),
+		 	{ok, St}
+	end
   end;
 
 %
@@ -131,7 +180,7 @@ loop(St, {nick, Nick}) ->
     false ->
       {{error, user_already_connected, "We are already connected, we can't change username when we are connected"}, St};
     true ->
-      {ok, create_state(Nick, St#client_st.gui, St#client_st.serverName, St#client_st.list_chatRoom)}
+      {ok, create_state(Nick, St#client_st.gui, St#client_st.serverName,St#client_st.machineName, St#client_st.list_chatRoom)}
   end;
 
 
--- Lab4/Solution/defs.hrl
+++ Lab4/Solution/defs.hrl
@@ -3,7 +3,7 @@
 % It contains the following fields:
 
 %   gui: the name (or Pid) of the GUI process.
--record(client_st, {gui,nickname, serverName,list_chatRoom}).
+-record(client_st, {gui,nickname, serverName, machineName, list_chatRoom}).
 
 % This record defines the structure of the server process.
 % Add whatever other fields you need.
